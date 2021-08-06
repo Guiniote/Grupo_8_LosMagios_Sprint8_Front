5d3f1c1e81e2aa7d7f957bd5c08ffaf2e4508ad5
gustavomarin18 <marinproducciones81@gmail.com>
2021-08-05 21:02:41 -0300

    subo detalle producto-marin

---
+++ src/components/LastProduct.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+
+
+
+
+
+class LastProduct extends Component{
+    constructor(){
+        super()
+        this.state ={
+            productLast : {}
+        }
+    }
+    //Compomentes Ciclo de vida - Montar - Actualizar - Desmontar
+    //Montaje 
+    componentDidMount(){
+        fetch('http://localhost:3001/api/products')
+        .then(respuesta =>{
+
+            return respuesta.json()
+        })
+        .then(products =>{
+            let product= products.products
+            let last = product.pop()
+
+        
+         
+            this.setState({productLast: {
+                id : last.id,
+                name : last.name,
+                description: last.description,
+                image: last.images[0]
+            
+            }
+                
+                
+                
+               })
+           
+        })
+        .catch(error => console.log(error))
+
+    }
+
+
+
+
+    // fondo(){
+    //     let fondoCaja = document.querySelector('.fondoCaja');
+    //     fondoCaja.classList.toggle('bg-secondary');
+    // }
+
+
+    render(){
+        return (
+            <React.Fragment>
+            
+						{/*<!-- Last Movie in DB -->*/}
+						<div className="col-lg-6 mb-4">
+							<div className="card shadow mb-4">
+								<div className="card-header py-3">
+									<h5 className="m-0 font-weight-bold text-gray-800">Ultimo producto creado</h5>
+								</div>
+								<div className="card-body">
+									<div className="text-center">
+										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={this.state.productLast.image} alt=" Star Wars - Mandalorian "/>
+									</div>
+									<p>{this.state.productLast.description}</p> 
+									<a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
+								</div>
+							</div>
+						</div>
+                       
+
+
+
+
+
+
+             
+    
+    </React.Fragment>
+    )
+    }
+}
+export default LastProduct;
